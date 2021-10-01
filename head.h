1f91291d328f3f9bedf74bbb1ef8739391375c83
Saksham Joshi <85193238+saksham221201@users.noreply.github.com>
2021-10-01 10:28:20 +0530

    Update head.h

--- head.h
+++ head.h
@@ -8,6 +8,7 @@ int chk_ml(char str[])
    char *p,*q;
    int z,x,j;
    
+	
    p = strchr(str,'@');
    z=p-&str[0];
    z++;
@@ -15,10 +16,11 @@ int chk_ml(char str[])
    x=q-&str[0];
    x++;
    
+	
    j=q-p;
    
-   if(j>1)
-   {
+	
+   if(j>1){
    if(z>2)
    {system("color f2");
    return 1;
@@ -34,14 +36,14 @@ printf("\tInvalid Email");
 return 0;
 }}
 
-void pr_s(void)
-{
+
+void pr_s(void){
 	int l;
 	for(l=0;l<120;l++)
 	printf("_");	
 }
 
-void source(void)
-{	system("del data.txt");
+void source(void){
+	system("del data.txt");
 	rename("data2.txt","data.txt");
 	}
